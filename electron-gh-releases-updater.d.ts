0cb2676639097ab879481824bbe9cad7082b896b
Copper Phosphate <cu3po42@gmail.com>
2016-01-10 20:37:12 +0100

    Update TypeScript definition for changelog functionality

--- electron-gh-releases-updater.d.ts
+++ electron-gh-releases-updater.d.ts
@@ -1,4 +1,4 @@
 declare module "electron-gh-releases-updater" {
-    function check(packageJson: any, callback: (err, res: {updateAvailable: boolean, update?: (directory: string, callback: (err) => void) => void}) => void): void;
+function check(packageJson: any, callback: (err, res: {updateAvailable: boolean, changelog: {tag: string, name: string, body: string}[], update?: (directory: string, callback: (err) => void) => void}) => void): void;
     export = check;
 }
