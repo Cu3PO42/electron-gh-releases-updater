fa0d510a570b4ea0911ded3e4bb3f1905a1b0014
Copper Phosphate <cu3po42@gmail.com>
2016-01-16 16:19:46 +0100

    Update type definitions to new style

--- electron-gh-releases-updater.d.ts
+++ electron-gh-releases-updater.d.ts
@@ -1,4 +1,23 @@
-declare module "electron-gh-releases-updater" {
-function check(packageJson: any, callback: (err, res: {updateAvailable: boolean, changelog: {tag: string, name: string, body: string}[], update?: (callback: (err) => void) => void}) => void, progressCallback: (progress: {percentage: number, speed: number, size: {total: number, transferred: number}, time: {elapsed: number, remaining: number}}) => void): void;
-    export = check;
-}
+declare function check(
+    packageJson: any,
+    callback: (
+        err,
+        res: {
+            updateAvailable: boolean,
+            changelog: {
+                tag: string,
+                name: string,
+                body: string
+            }[],
+            update?: (callback: (err) => void) => void
+        }) => void,
+    progressCallback: (
+        progress: {
+            percentage: number,
+            speed: number,
+            size: {total: number, transferred: number},
+            time: {elapsed: number, remaining: number}
+        }) => void
+    ): void;
+    
+export = check;
