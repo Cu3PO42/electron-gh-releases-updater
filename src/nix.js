80b04d3adfc0215a324d1be97a3349d384d3a528
Copper Phosphate <cu3po42@gmail.com>
2016-06-19 16:45:09 +0200

    Fix full update on *nix

--- src/nix.js
+++ src/nix.js
@@ -2,13 +2,14 @@ import * as fse from 'fs-extra';
 import { spawn } from 'child_process';
 import Promise from 'bluebird';
 import { app } from 'electron';
+import { join, relative } from 'path';
 
 const moveAsync = Promise.promisify(fse.move);
 const writeFileAsync = Promise.promisify(fse.writeFile);
 
 export function unzip(zipFile, destination) {
   return new Promise((resolve) => {
-    spawn('unzip', [zipFile, '-d', destination]).on('close', resolve);
+    spawn('unzip', [zipFile, '-d', destination], { stdio: 'ignore' }).on('close', resolve);
   });
 }
 
@@ -18,14 +19,21 @@ export function restart() {
     `while kill -0 "${process.pid}"; do sleep 1; done; "${process.execPath}" &`
   ], {
     detached: true,
-    stdio: "ignore",
+    stdio: 'ignore',
     cwd: process.cwd()
   }).unref();
   app.quit();
 }
 
 export async function doFullUpdate(appPath, newPath, currentVersion) {
-  await moveAsync(newPath, appPath, { clobber: true });
-  await writeFileAsync(path.join(process.resourcesPath, 'UPDATED'), currentVersion, 'utf-8');
-  restart();
+  await writeFileAsync(join(newPath, relative(appPath, process.resourcesPath), 'UPDATED'), currentVersion, 'utf-8');
+  spawn('sh', [
+    '-c',
+    `while kill -0 "${process.pid}"; do sleep 1; done; rm -rf "${appPath}"; mv "${newPath}" "${appPath}"; "${process.execPath}" &`
+  ], {
+    detached: true,
+    stdio: 'ignore',
+    cwd: process.cwd()
+  }).unref();
+  app.quit();
 }
